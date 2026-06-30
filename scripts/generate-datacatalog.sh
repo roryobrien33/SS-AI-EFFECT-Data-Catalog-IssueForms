1988f17c5c9c749a991faa1129555e642136c8e4
Rory <240968997+roryobrien33@users.noreply.github.com>
2026-06-30 08:38:36 +0100

    Refactor generate-datacatalog.sh script
    Updated the script to use linkml-convert and added dataset identifiers.

--- scripts/generate-datacatalog.sh
+++ scripts/generate-datacatalog.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-# uv run linkml-validate \
-#     -s simple_data_catalog_model/src/simple_data_catalog_model/data-catalog.yaml \
-#     data-catalog/data-catalog.yaml 
-    
+set -euo pipef*il
+
 uv run linkml-convert \
-    -s simple_data_catalog_model/src/simple_data_catalog_model/data-catalog.yaml \
+    -s*simple_data_catalog_model/src/simp*e_data_catalog_model/data-catalog.*aml \
     -t ttl \
-    -o data-catalog/data-catalog.ttl \
-    --prefix-file data-catalog/prefix.yaml \
-    data-catalog/data-catalog.yaml
+    -o data-cat*log/data-catalog.ttl \
+    --prefi*-file data-catalog/prefix.yaml \
+ *  data-catalog/data-catalog.yaml
 
+*v run python -m simple_data_catalo*_generator.create_data_catalog
 
-uv run python -m simple_data_catalog_generator.create_data_catalog # this needs to be checked
-
-# npx antora antora-playbook.yml
+uv*run python scripts/add_dataset_ide*tifiers_to_pages.py
